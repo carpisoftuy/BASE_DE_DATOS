a2a2b2ab5fc7d0c21407f72a76a178a1a1973d81
Dino <92330888+MomoMamado@users.noreply.github.com>
2023-09-15 16:00:29 -0300

    Update pasaje.sql
    Corregidos algunos errores

--- pasaje.sql
+++ pasaje.sql
@@ -1,46 +1,50 @@
+-- las PK son todas simples porque laravel lo requiere
+-- en los casos en los que la PK no era simple se agregó una id y la PK antigua se convirtió en una clave única
+
 use carpisoft;
 create table usuario(
-id_usuario bigint unsigned auto_increment not null,
-username varchar(32) unique not null,
-password varchar (256) not null,
+id bigint unsigned auto_increment not null,
+username varchar(32) not null,
+password varchar(256) not null,
 nombre varchar(32) not null,
 apellido varchar(32) not null,
-primary key (id_usuario)
+primary key (id),
+UNIQUE (username)
 );
 
 create table informacion(
 id bigint unsigned auto_increment not null,
 id_usuario bigint unsigned not null,
-tipo varchar(16) unique not null,
-detalle varchar (256) not null,
+tipo varchar(16) not null,
+detalle varchar(256) not null,
 primary key (id),
-FOREIGN KEY (id_usuario) REFERENCES usuario(id_usuario)
+FOREIGN KEY (id_usuario) REFERENCES usuario(id)
 );
 
 create table administrador(
-id bigint unsigned auto_increment not null,
+id bigint unsigned not null,
 primary key (id),
-FOREIGN KEY (id) REFERENCES usuario(id_usuario)
+FOREIGN KEY (id) REFERENCES usuario(id)
 );
 
 create table chofer(
-id bigint unsigned auto_increment not null,
+id bigint unsigned not null,
 primary key (id),
-FOREIGN KEY (id) REFERENCES usuario(id_usuario)
+FOREIGN KEY (id) REFERENCES usuario(id)
 );
 
 create table almacenero(
-id bigint unsigned auto_increment not null,
+id bigint unsigned not null,
 primary key (id),
-FOREIGN KEY (id) REFERENCES usuario(id_usuario)
+FOREIGN KEY (id) REFERENCES usuario(id)
 );
 
 create table ubicacion(
 id bigint unsigned auto_increment not null,
-direccion varchar(256)not null,
-codigo_postal varchar(8)not null,
-latitud decimal(10,8)not null,
-longuitud decimal(10,8)not null,
+direccion varchar(256) not null,
+codigo_postal varchar(8) not null,
+latitud decimal(10,8) not null,
+longitud decimal(10,8) not null,
 primary key (id)
 );
 
@@ -55,16 +59,17 @@ FOREIGN KEY (id_ubicacion) REFERENCES ubicacion(id)
 
 create table gestiona(
 id bigint unsigned auto_increment not null,
-id_usuario bigint unsigned not null unique,
-fecha_inicio timestamp unique not null,
+id_usuario bigint unsigned not null,
+fecha_inicio timestamp not null,
 id_almacen bigint unsigned not null,
 primary key (id),
+UNIQUE (id_usuario, fecha_inicio),
 FOREIGN KEY (id_usuario) REFERENCES almacenero(id),
 FOREIGN KEY (id_almacen) REFERENCES almacen(id)
 );
 
 create table gestiona_fin(
-id bigint unsigned auto_increment not null,
+id bigint unsigned not null,
 fecha_fin timestamp not null,
 primary key (id),
 FOREIGN KEY (id) REFERENCES gestiona(id)
@@ -81,7 +86,7 @@ FOREIGN KEY (almacen_destino) REFERENCES almacen(id)
 );
 
 create table bulto_desarmado(
-id bigint unsigned auto_increment not null,
+id bigint unsigned not null,
 fecha_desarmado timestamp not null,
 primary key (id),
 FOREIGN KEY (id) REFERENCES bulto(id)
@@ -96,7 +101,7 @@ primary key (id)
 );
 
 create table paquete_para_recoger(
-id bigint unsigned auto_increment not null,
+id bigint unsigned not null,
 almacen_destino bigint unsigned not null,
 primary key (id),
 FOREIGN KEY (id) REFERENCES paquete(id),
@@ -104,22 +109,22 @@ FOREIGN KEY (almacen_destino) REFERENCES almacen(id)
 );
 
 create table paquete_recogido(
-id bigint unsigned auto_increment not null,
+id bigint unsigned not null,
 fecha_recogido timestamp not null,
 primary key (id),
 FOREIGN KEY (id) REFERENCES paquete_para_recoger(id)
 );
 
 create table paquete_para_entregar(
-id bigint unsigned auto_increment not null,
+id bigint unsigned not null,
 ubicacion_destino bigint unsigned not null,
 primary key (id),
 FOREIGN KEY (id) REFERENCES paquete(id),
-FOREIGN KEY (id) REFERENCES ubicacion(id)
+FOREIGN KEY (ubicacion_destino) REFERENCES ubicacion(id)
 );
 
 create table paquete_entregado(
-id bigint unsigned auto_increment not null,
+id bigint unsigned not null,
 fecha_entregado timestamp not null,
 primary key (id),
 FOREIGN KEY (id) REFERENCES paquete_para_entregar(id)
@@ -127,16 +132,17 @@ FOREIGN KEY (id) REFERENCES paquete_para_entregar(id)
 
 create table almacen_contiene_bulto(
 id bigint unsigned auto_increment not null,
-id_bulto bigint unsigned unique not null,
-fecha_inicio timestamp unique not null,
+id_bulto bigint unsigned not null,
+fecha_inicio timestamp not null,
 id_almacen bigint unsigned not null,
 primary key (id),
+UNIQUE(id_bulto, fecha_inicio),
 FOREIGN KEY (id_bulto) REFERENCES bulto(id),
 FOREIGN KEY (id_almacen) REFERENCES almacen(id)
 );
 
 create table almacen_contiene_bulto_fin(
-id bigint unsigned auto_increment not null,
+id bigint unsigned not null,
 fecha_fin timestamp not null,
 primary key (id),
 FOREIGN KEY (id) REFERENCES almacen_contiene_bulto(id)
@@ -144,16 +150,17 @@ FOREIGN KEY (id) REFERENCES almacen_contiene_bulto(id)
 
 create table almacen_contiene_paquete(
 id bigint unsigned auto_increment not null,
-id_paquete bigint unsigned unique not null,
-fecha_inicio timestamp unique not null,
+id_paquete bigint unsigned not null,
+fecha_inicio timestamp not null,
 id_almacen bigint unsigned not null,
 primary key (id),
+UNIQUE(id_paquete, fecha_inicio),
 FOREIGN KEY (id_paquete) REFERENCES paquete(id),
 FOREIGN KEY (id_almacen) REFERENCES almacen(id)
 );
 
 create table almacen_contiene_paquete_fin(
-id bigint unsigned auto_increment not null,
+id bigint unsigned not null,
 fecha_fin timestamp not null,
 primary key (id),
 FOREIGN KEY (id) REFERENCES almacen_contiene_paquete(id)
@@ -161,8 +168,8 @@ FOREIGN KEY (id) REFERENCES almacen_contiene_paquete(id)
 
 create table bulto_contiene(
 id bigint unsigned auto_increment not null,
-id_paquete bigint unsigned unique not null,
-fecha_inicio timestamp unique not null,
+id_paquete bigint unsigned not null,
+fecha_inicio timestamp not null,
 id_bulto bigint unsigned not null,
 primary key (id),
 FOREIGN KEY (id_paquete) REFERENCES paquete(id),
@@ -170,7 +177,7 @@ FOREIGN KEY (id_bulto) REFERENCES bulto(id)
 );
 
 create table bulto_contiene_fin(
-id bigint unsigned auto_increment not null,
+id bigint unsigned not null,
 fecha_fin timestamp not null,
 primary key (id),
 FOREIGN KEY (id) REFERENCES bulto_contiene(id)
@@ -178,33 +185,35 @@ FOREIGN KEY (id) REFERENCES bulto_contiene(id)
 
 create table vehiculo(
 id bigint unsigned auto_increment not null,
-codigo_postal varchar(3) unique not null,
-matricula varchar(16) unique not null,
+codigo_postal varchar(3) not null,
+matricula varchar(16) not null,
 capacidad_volumen int not null,
 capacidad_peso int not null,
 peso_ocupado int not null,
 volumen_ocupado int not null,
-primary key (id)
+primary key (id),
+UNIQUE(codigo_pais, matricula)
 );
 
 create table camion(
-id bigint unsigned auto_increment not null,
+id bigint unsigned not null,
 primary key (id),
 FOREIGN KEY (id) REFERENCES vehiculo(id)
 );
 
 create table camioneta(
-id bigint unsigned auto_increment not null,
+id bigint unsigned not null,
 primary key (id),
 FOREIGN KEY (id) REFERENCES vehiculo(id)
 );
 
 create table maneja(
 id bigint unsigned auto_increment not null,
-id_vehiculo bigint unsigned unique not null,
-id_usuario bigint unsigned unique not null,
-fecha_inicio timestamp unique not null,
+id_vehiculo bigint unsigned not null,
+id_usuario bigint unsigned not null,
+fecha_inicio timestamp not null,
 primary key (id),
+UNIQUE(id_vehiculo, fecha_inicio)
 FOREIGN KEY (id_vehiculo) REFERENCES vehiculo(id),
 FOREIGN KEY (id_usuario) REFERENCES chofer(id)
 );
@@ -218,10 +227,11 @@ FOREIGN KEY (id) REFERENCES chofer(id)
 
 create table carga_bulto(
 id bigint unsigned auto_increment not null,
-id_bulto bigint unsigned unique not null,
-fecha_inicio timestamp unique not null,
+id_bulto bigint unsigned not null,
+fecha_inicio timestamp not null,
 id_vehiculo bigint unsigned not null,
 primary key (id),
+UNIQUE(id_bulto, fecha_inicio),
 FOREIGN KEY (id_bulto) REFERENCES bulto(id),
 FOREIGN KEY (id_vehiculo) REFERENCES camion(id)
 );
