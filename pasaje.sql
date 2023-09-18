bc87e019a1cc7ba7c4b6d181b2c8f06df7da4820
Martín Sosa <110544650+sosam1@users.noreply.github.com>
2023-09-18 20:27:10 -0300

    Update pasaje.sql

--- pasaje.sql
+++ pasaje.sql
@@ -1,5 +1,6 @@
-create database carpisoft;
-use carpisoft;
+drop database ejemplo;
+create database ejemplo;
+use ejemplo;
 
 create table usuario(
 id bigint unsigned auto_increment not null,
@@ -259,14 +260,3 @@ fecha_fin timestamp not null,
 primary key (id),
 FOREIGN KEY (id) REFERENCES carga_paquete(id)
 );
-
-SELECT u.username, v.matricula, p.*
-FROM usuario AS u
-INNER JOIN chofer AS c ON u.id = c.id
-INNER JOIN maneja AS m ON c.id = m.id_usuario
-INNER JOIN vehiculo AS v ON m.id_vehiculo = v.id
-INNER JOIN carga_paquete AS cp ON v.id = cp.id_vehiculo
-INNER JOIN paquete AS p ON cp.id_paquete = p.id
-INNER JOIN paquete_entregado AS pe ON p.id = pe.id
-WHERE c.id = 1;
-
