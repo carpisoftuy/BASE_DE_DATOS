f7503a216e267d493f8bfdcd3e1ab4b28836a207
Martín Sosa <110544650+sosam1@users.noreply.github.com>
2023-09-28 16:08:23 -0300

    Update consultas.sql

--- consultas.sql
+++ consultas.sql
@@ -1,3 +1,4 @@
+#Selecciona el nombre de usuario de los choferes, la matricula de los vehiculos en los que se cargó y los datos del paquete entregado con id 2
 SELECT u.username, v.matricula, p.*
 FROM usuario AS u
 INNER JOIN chofer AS c ON u.id = c.id
@@ -6,4 +7,4 @@ INNER JOIN vehiculo AS v ON m.id_vehiculo = v.id
 INNER JOIN carga_paquete AS cp ON v.id = cp.id_vehiculo
 INNER JOIN paquete AS p ON cp.id_paquete = p.id
 INNER JOIN paquete_entregado AS pe ON p.id = pe.id
-WHERE c.id = 1;
+WHERE p.id = 2;
