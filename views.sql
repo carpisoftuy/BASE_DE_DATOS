2d4b117e5c9b168f7517e3cdadaaf3235b875103
Martín Sosa <110544650+sosam1@users.noreply.github.com>
2023-10-01 18:06:55 -0300

    Create views.sql

---
+++ views.sql
@@ -0,0 +1,13 @@
+# Seleccionar la id de los paquetes que fueron entregados, los choferes que llevaron dichos paquetes, en que fechas y la matricula del camion
+CREATE VIEW vista_reporte_entregas AS
+SELECT p.id, u.username, u.nombre, u.apellido, v.matricula, cp.fecha_inicio, cpf.fecha_fin
+FROM usuario AS u
+INNER JOIN chofer AS c ON u.id = c.id
+INNER JOIN maneja AS m ON c.id = m.id_usuario
+INNER JOIN vehiculo AS v ON m.id_vehiculo = v.id
+INNER JOIN carga_paquete AS cp ON v.id = cp.id_vehiculo
+INNER JOIN carga_paquete_fin AS cpf ON cp.id = cpf.id
+INNER JOIN paquete AS p ON cp.id_paquete = p.id
+INNER JOIN paquete_entregado AS pe ON p.id = pe.id;
+
+select * from vista_reporte_entregas;
